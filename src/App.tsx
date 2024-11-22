aa45e19f6ab7ef1122f683dd1202d7150f43ccd0
Adel Ahmed <81045048+adelahmed0@users.noreply.github.com>
2024-11-22 15:48:21 +0200

    add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import './App.css';
 import ProductCard from './components/ProductCard.tsx';
 import { productList } from './data/data.ts';
+import Modal from './components/ui/Modal.tsx';
+import { useState } from 'react';
+import Button from './components/ui/Button.tsx';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  // function openModal() {
+  //   setIsOpen(true);
+  // }
+
   const renderProductCard = productList.map((product) => {
     return <ProductCard key={product.id} product={product} />;
   });
@@ -11,6 +24,12 @@ function App() {
       <div className=" m-5 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md">
         {renderProductCard}
       </div>
+      <Modal closeModal={closeModal} isOpen={isOpen}>
+        <div className="flex items-center space-x-2">
+          <Button className="bg-indigo-700 hover:bg-indigo-600">Submit</Button>
+          <Button className="bg-gray-300 hover:bg-gray-400">Cancel</Button>
+        </div>
+      </Modal>
     </main>
   );
 }
---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,45 @@
+import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
+import { ReactNode } from 'react';
+
+interface IProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  title?: string;
+  children: ReactNode;
+}
+
+const Modal = ({ isOpen, closeModal, title, children }: IProps) => {
+  return (
+    <div>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <Dialog
+          open={isOpen}
+          as="div"
+          className="relative z-10 focus:outline-none"
+          onClose={closeModal}
+        >
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-black bg-opacity-25">
+            <div className="flex min-h-full items-center justify-center p-4">
+              <DialogPanel
+                transition
+                className="w-full max-w-md rounded-xl bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+              >
+                {title && (
+                  <DialogTitle
+                    as="h3"
+                    className="text-base/7 font-medium text-white"
+                  >
+                    {title}
+                  </DialogTitle>
+                )}
+                <div className="mt-4">{children}</div>
+              </DialogPanel>
+            </div>
+          </div>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
