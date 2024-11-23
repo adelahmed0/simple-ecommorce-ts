d10b8fd8a006e6f270e260e9615396f99d6cba74
Adel Ahmed <81045048+adelahmed0@users.noreply.github.com>
2024-11-23 17:50:45 +0200

    form submit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import './App.css';
 import ProductCard from './components/ProductCard.tsx';
 import { formInputsList, productList } from './data/data.ts';
 import Modal from './components/ui/Modal.tsx';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import Button from './components/ui/Button.tsx';
 import Input from './components/ui/Input.tsx';
 import { IProduct } from './interfaces/interfaces.ts';
 
 function App() {
-  const [product, setProduct] = useState<IProduct>({
+  const defaultProductObj = {
     title: '',
     description: '',
     imageURL: '',
@@ -18,19 +18,28 @@ function App() {
       name: '',
       imageURL: '',
     },
-  });
-  const [isOpen, setIsOpen] = useState(true);
+  };
+  const [product, setProduct] = useState<IProduct>(defaultProductObj);
+  const [isOpen, setIsOpen] = useState(false);
 
   const closeModal = () => setIsOpen(false);
   const openModal = () => setIsOpen(true);
-
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setProduct((prevState) => ({
       ...prevState,
       [name]: value,
     }));
+  };
+
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     console.log(product);
+  };
+
+  const onCancel = () => {
+    setProduct(defaultProductObj);
+    closeModal();
   };
 
   const renderProductCard = productList.map((product) => {
@@ -39,7 +48,7 @@ function App() {
 
   const renderFormInputList = formInputsList.map((formInput) => {
     return (
-      <div className="flex flex-col">
+      <div className="flex flex-col" key={formInput.id}>
         <label
           htmlFor={formInput.id}
           className="text-sm font-medium text-gray-700 mb-[2px]"
@@ -66,13 +75,18 @@ function App() {
         {renderProductCard}
       </div>
       <Modal closeModal={closeModal} isOpen={isOpen} title="Add a new product">
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={submitHandler}>
           {renderFormInputList}
           <div className="flex items-center space-x-3">
             <Button className="bg-indigo-700 hover:bg-indigo-600">
               Submit
             </Button>
-            <Button className="bg-gray-300 hover:bg-gray-400">Cancel</Button>
+            <Button
+              className="bg-gray-300 hover:bg-gray-400"
+              onClick={onCancel}
+            >
+              Cancel
+            </Button>
           </div>
         </form>
       </Modal>
